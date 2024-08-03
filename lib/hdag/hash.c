6d711d9707501132e4733f54c2c80bb741b3a725
Nikolai Kondrashov <spbnick@gmail.com>
2024-08-03 21:25:16 +0300

    Add forgotted lib/hdag/hash.c

---
+++ lib/hdag/hash.c
@@ -0,0 +1,12 @@
+/*
+ * Hash DAG node ID hash
+ */
+
+#include <hdag/hash.h>
+#include <string.h>
+
+int
+hdag_hash_cmp(const void *a, const void *b, void *plen)
+{
+    return memcmp(a, b, *(uint16_t *)plen);
+}
