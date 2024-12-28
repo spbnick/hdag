6633e8c0f9414fed413a5334fd4d5458192c2a32
Nikolai Kondrashov <spbnick@gmail.com>
2024-12-29 01:47:42 +0200

    hash_seq: Add direct reset/next funcs

--- include/hdag/hash_seq.h
+++ include/hdag/hash_seq.h
@@ -81,6 +81,40 @@ hdag_hash_seq_is_resettable(const struct hdag_hash_seq *hash_seq)
     return hash_seq->reset_fn != NULL;
 }
 
+/**
+ * Reset a hash sequence to the start position.
+ *
+ * @param hash_seq  Hash sequence to reset. Must be resettable.
+ */
+static inline void
+hdag_hash_seq_reset(const struct hdag_hash_seq *hash_seq)
+{
+    assert(hdag_hash_seq_is_valid(hash_seq));
+    assert(hdag_hash_seq_is_resettable(hash_seq));
+    hash_seq->reset_fn(hash_seq);
+}
+
+/**
+ * Get the next hash out of a sequence, if possible.
+ *
+ * @param hash_seq  The sequence to retrieve the next hash from.
+ * @param phash     Location for the retrieved hash.
+ *                  The length of the hash is defined in hash_seq.
+ *                  Can be modified even in case of failure.
+ *
+ * @return  Zero (HDAG_RES_OK) if the hash was retrieved successfully.
+ *          A positive number if there were no more hashes.
+ *          A negative number (a failure result) if hash retrieval has failed.
+ */
+static inline hdag_res
+hdag_hash_seq_next(const struct hdag_hash_seq *hash_seq,
+                   uint8_t *phash)
+{
+    assert(hdag_hash_seq_is_valid(hash_seq));
+    assert(phash != NULL);
+    return hash_seq->next_fn(hash_seq, phash);
+}
+
 /** A next-hash retrieval function which never returns hashes */
 [[nodiscard]]
 extern hdag_res hdag_hash_seq_empty_next_fn(
