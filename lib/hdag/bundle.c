e1308cead863be894e7041813c1bca1759334d94
Nikolai Kondrashov <spbnick@gmail.com>
2025-01-19 20:17:43 +0200

    bundle: Drop node forgetting as it's wrong

--- lib/hdag/bundle.c
+++ lib/hdag/bundle.c
@@ -1858,33 +1858,3 @@ cleanup:
     hdag_bundle_cleanup(&bundle);
     return HDAG_RES_ERRNO_IF_INVALID(res);
 }
-
-hdag_res
-hdag_bundle_node_forget(struct hdag_bundle *bundle, uint32_t node_idx)
-{
-    struct hdag_node *node;
-    size_t hash_idx;
-    bool found;
-
-    assert(hdag_bundle_is_valid(bundle));
-    assert(hdag_bundle_is_mutable(bundle));
-    assert(node_idx < hdag_darr_occupied_slots(&bundle->nodes));
-
-    node = hdag_darr_element(&bundle->nodes, node_idx);
-    if (hdag_node_is_known(node)) {
-        /* Add the node hash to unknown hashes */
-        found = hdag_hashes_darr_find(&bundle->unknown_hashes,
-                                      node->hash, &hash_idx);
-        assert(!found);
-        if (!found) {
-            if (hdag_darr_insert_one(&bundle->unknown_hashes,
-                                     hash_idx, node->hash) == NULL) {
-                return HDAG_RES_ERRNO;
-            }
-        }
-        /* Mark the node unknown */
-        node->targets = HDAG_TARGETS_UNKNOWN;
-    }
-
-    return HDAG_RES_OK;
-}
