66cb9110cab2147ef39ed8b93c330407d5003951
Nikolai Kondrashov <spbnick@gmail.com>
2025-02-22 19:49:39 +0200

    Extract hdagt-darr test

---
+++ src/hdagt/hdagt-darr.c
@@ -0,0 +1,67 @@
+/*
+ * Hash DAG database dynamic array tests
+ */
+
+#include <hdag/misc.h>
+#include <hdag/darr.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+#define TEST(_expr) \
+    do {                                                \
+        if (!(_expr)) {                                 \
+            fprintf(stderr, "%s:%u: Test failed: %s\n", \
+                    __FILE__, __LINE__, #_expr);        \
+            failed++;                                   \
+        }                                               \
+    } while(0)
+
+static size_t
+test(void)
+{
+    size_t failed = 0;
+    struct hdag_darr darr;
+
+    /* Test allocating boundary number of elements */
+    darr = HDAG_DARR_EMPTY(1, 1);
+    TEST(hdag_darr_alloc(&darr, 0) == NULL);
+    TEST(hdag_darr_alloc(&darr, 1) != NULL);
+    TEST(hdag_darr_uappend(&darr, 0) == NULL);
+    hdag_darr_cleanup(&darr);
+
+    /* Test void array operations */
+    darr = HDAG_DARR_EMPTY(0, 16);
+    TEST(hdag_darr_is_valid(&darr));
+    TEST(hdag_darr_is_void(&darr));
+    TEST(hdag_darr_is_clean(&darr));
+    TEST(hdag_darr_occupied_slots(&darr) == 0);
+    TEST(hdag_darr_occupied_size(&darr) == 0);
+    TEST(hdag_darr_allocated_slots(&darr) == 0);
+    TEST(hdag_darr_allocated_size(&darr) == 0);
+    TEST(hdag_darr_is_empty(&darr));
+    hdag_darr_empty(&darr);
+    TEST(hdag_darr_is_valid(&darr));
+    TEST(hdag_darr_is_void(&darr));
+    TEST(hdag_darr_is_empty(&darr));
+    hdag_darr_cleanup(&darr);
+    TEST(hdag_darr_is_valid(&darr));
+    TEST(hdag_darr_is_void(&darr));
+    TEST(hdag_darr_is_clean(&darr));
+    TEST(hdag_darr_alloc(&darr, 0) == NULL);
+    TEST(hdag_darr_deflate(&darr));
+    TEST(hdag_darr_uappend(&darr, 0) == NULL);
+
+    return failed;
+}
+
+int
+main(void)
+{
+    size_t failed = test();
+    if (failed) {
+        fprintf(stderr, "%zu tests failed.\n", failed);
+    }
+    return failed != 0;
+}
--- src/hdagt/hdagt-misc.c
+++ src/hdagt/hdagt-misc.c
@@ -26,42 +26,12 @@ test(void)
     uint8_t hash[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef};
     char hex_buf[sizeof(hash) * 2 + 1];
 
+    /* Test hdag_bytes_to_hex */
     TEST(hdag_bytes_to_hex(hex_buf, hash, sizeof(hash)) == hex_buf);
     TEST(strcmp(hex_buf, "0123456789abcdef") == 0);
+    TEST(hdag_bytes_to_hex(hex_buf, NULL, 0) == hex_buf);
 
-    {
-        struct hdag_darr darr = HDAG_DARR_EMPTY(1, 1);
-
-        TEST(hdag_darr_alloc(&darr, 0) == NULL);
-        TEST(hdag_darr_alloc(&darr, 1) != NULL);
-        TEST(hdag_darr_uappend(&darr, 0) == NULL);
-        hdag_darr_cleanup(&darr);
-    }
-
-    {
-        struct hdag_darr darr = HDAG_DARR_EMPTY(0, 16);
-
-        TEST(hdag_darr_is_valid(&darr));
-        TEST(hdag_darr_is_void(&darr));
-        TEST(hdag_darr_is_clean(&darr));
-        TEST(hdag_darr_occupied_slots(&darr) == 0);
-        TEST(hdag_darr_occupied_size(&darr) == 0);
-        TEST(hdag_darr_allocated_slots(&darr) == 0);
-        TEST(hdag_darr_allocated_size(&darr) == 0);
-        TEST(hdag_darr_is_empty(&darr));
-        hdag_darr_empty(&darr);
-        TEST(hdag_darr_is_valid(&darr));
-        TEST(hdag_darr_is_void(&darr));
-        TEST(hdag_darr_is_empty(&darr));
-        hdag_darr_cleanup(&darr);
-        TEST(hdag_darr_is_valid(&darr));
-        TEST(hdag_darr_is_void(&darr));
-        TEST(hdag_darr_is_clean(&darr));
-        TEST(hdag_darr_alloc(&darr, 0) == NULL);
-        TEST(hdag_darr_deflate(&darr));
-        TEST(hdag_darr_uappend(&darr, 0) == NULL);
-    }
-
+    /* Test HDAG_CONTAINER_OF() */
     {
         struct member {
             int b;
